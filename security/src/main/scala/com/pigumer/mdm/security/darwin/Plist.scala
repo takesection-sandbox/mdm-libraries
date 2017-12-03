a87b90906ceb2286d73d16009c4c93d240c8f221
Shigeki Shoji <takesection@gmail.com>
2017-12-03 23:45:27 +0900

    add Plist

---
+++ security/src/main/scala/com/pigumer/mdm/security/darwin/Plist.scala
@@ -0,0 +1,25 @@
+package com.pigumer.mdm.security.darwin
+
+class Plist {
+  val csr = ""
+  val chain = ""
+  val signature = ""
+  val plist =
+    <plist version="1.0">
+      <dict>
+        <key>PushCertRequestCSR</key>
+        <string>
+          { csr }
+        </string>
+        <key>PushCertCertificateChain</key>
+        <string>
+          { chain }
+        </string>
+        <key>PushCertSignature</key>
+        <string>
+          { signature }
+        </string>
+      </dict>
+    </plist>
+
+}
