b7cccb28b367d651540785fc331bf715e21a90da
Shigeki Shoji <takesection@gmail.com>
2017-12-11 23:57:35 +0900

    support S/MIME

--- security/src/main/scala/com/pigumer/mdm/security/Certificate.scala
+++ security/src/main/scala/com/pigumer/mdm/security/Certificate.scala
@@ -1,39 +1,20 @@
 package com.pigumer.mdm.security
 
-import java.io.StringWriter
+import java.io.{Reader, StringWriter}
 import java.util.Date
 
 import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers
 import org.bouncycastle.asn1.x500.X500Name
 import org.bouncycastle.asn1.x509.{AlgorithmIdentifier, SubjectPublicKeyInfo}
-import org.bouncycastle.cert.X509v3CertificateBuilder
+import org.bouncycastle.cert.{X509CertificateHolder, X509v3CertificateBuilder}
 import org.bouncycastle.crypto.util.PrivateKeyFactory
-import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator
+import org.bouncycastle.openssl.{PEMKeyPair, PEMParser}
+import org.bouncycastle.openssl.jcajce.{JcaMiscPEMGenerator, JcaPEMKeyConverter}
 import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder
 import org.bouncycastle.util.io.pem.PemWriter
 
-class Certificate(issuer: X500Name,
-                  serial: BigInt,
-                  notBefore: Date,
-                  notAfter: Date,
-                  subject: X500Name,
-                  keyPair: KeyPair) {
-
-  private val x509Certificate = {
-    val builder = new X509v3CertificateBuilder(issuer,
-      serial.bigInteger,
-      notBefore,
-      notAfter,
-      subject,
-      SubjectPublicKeyInfo.getInstance(keyPair.publicKey.getEncoded)
-    )
-    val sigAlgId = new AlgorithmIdentifier(OIWObjectIdentifiers.sha1WithRSA)
-    val digAlgId = new AlgorithmIdentifier(OIWObjectIdentifiers.idSHA1)
-    val privateKeyParam = PrivateKeyFactory.createKey(keyPair.privateKey.getEncoded)
-    val contentSigner = new BcRSAContentSignerBuilder(sigAlgId, digAlgId).build(privateKeyParam)
-    builder.build(contentSigner)
-  }
-
+trait X509Certificate {
+  val x509Certificate: X509CertificateHolder
   def toPEMString: String = {
     val writer = new StringWriter
     try {
@@ -49,6 +30,37 @@ class Certificate(issuer: X500Name,
     }
     finally {
       writer.close
+    }
+  }
+}
+
+object X509Certificate {
+  def build(issuer: X500Name,
+            serial: BigInt,
+            notBefore: Date,
+            notAfter: Date,
+            subject: X500Name,
+            keyPair: KeyPair): X509Certificate = {
+    val builder = new X509v3CertificateBuilder(issuer,
+      serial.bigInteger,
+      notBefore,
+      notAfter,
+      subject,
+      SubjectPublicKeyInfo.getInstance(keyPair.publicKey.getEncoded)
+    )
+    val sigAlgId = new AlgorithmIdentifier(OIWObjectIdentifiers.sha1WithRSA)
+    val digAlgId = new AlgorithmIdentifier(OIWObjectIdentifiers.idSHA1)
+    val privateKeyParam = PrivateKeyFactory.createKey(keyPair.privateKey.getEncoded)
+    val contentSigner = new BcRSAContentSignerBuilder(sigAlgId, digAlgId).build(privateKeyParam)
+    new X509Certificate {
+      override val x509Certificate = builder.build(contentSigner)
+    }
+  }
+  def parse(reader: Reader): X509Certificate = {
+    val parser = new PEMParser(reader)
+    val obj = parser.readObject
+    new X509Certificate {
+      override val x509Certificate = obj.asInstanceOf[X509CertificateHolder]
     }
   }
 }
--- security/src/main/scala/com/pigumer/mdm/security/SMimeMessage.scala
+++ security/src/main/scala/com/pigumer/mdm/security/SMimeMessage.scala
@@ -4,18 +4,19 @@ import java.io.InputStream
 import javax.mail.Session
 import javax.mail.internet.{MimeBodyPart, MimeMessage}
 
-import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
+import org.bouncycastle.cms.jcajce.{JceKeyTransEnvelopedRecipient, JceKeyTransRecipientId}
 import org.bouncycastle.mail.smime.{SMIMEEnveloped, SMIMEUtil}
 
-import scala.collection.JavaConverters._
-
 trait SMimeMessage {
-  def decrypt(inputStream: InputStream, key: PrivateKey): MimeBodyPart = {
+  def decrypt(inputStream: InputStream, certificate: X509Certificate, key: PrivateKey): MimeBodyPart = {
     val session = Session.getDefaultInstance(System.getProperties, null)
     val message = new MimeMessage(session, inputStream)
     val enveloped = new SMIMEEnveloped(message)
+    val recipientId = new JceKeyTransRecipientId(
+      new JcaX509CertificateConverter().getCertificate(certificate.x509Certificate))
     val recipients = enveloped.getRecipientInfos
-    val recipient = recipients.getRecipients.asScala.head
+    val recipient = recipients.get(recipientId)
     val keyTransRecipient = new JceKeyTransEnvelopedRecipient(key.privateKey)
     SMIMEUtil.toMimeBodyPart(recipient.getContent(keyTransRecipient))
   }
